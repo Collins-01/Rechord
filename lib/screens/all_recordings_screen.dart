44fd538bc0c7ab7c8a0f64867106f7e03b7415c6
Collins Oriakhi <collins.oriakhi@itara.co>
2022-03-16 11:48:28 +0100

    Added the bottom player

--- lib/screens/all_recordings_screen.dart
+++ lib/screens/all_recordings_screen.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rechord/utils/app_colors.dart';
 
-class AllRecordingsScreen extends StatelessWidget {
+class AllRecordingsScreen extends StatefulWidget {
   const AllRecordingsScreen({Key? key}) : super(key: key);
 
+  @override
+  State<AllRecordingsScreen> createState() => _AllRecordingsScreenState();
+}
+
+class _AllRecordingsScreenState extends State<AllRecordingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,9 +61,101 @@ class AllRecordingsScreen extends StatelessWidget {
             ),
             Expanded(
               child: ListView.separated(
-                itemBuilder: (_, index) => Container(
-                  height: 70,
-                  color: Colors.red,
+                itemBuilder: (_, index) => SizedBox(
+                  // height: 70,
+                  child: ListTile(
+                    leading: IconButton(
+                      onPressed: () {
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (_) => Container(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.close,
+                                          color: Colors.white,
+                                          size: 24,
+                                        ),
+                                      ),
+                                      const Text(
+                                        'My Recording',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  // const Spacer(),
+                                  Container(
+                                    height: 90,
+                                    child: Row(
+                                      children: [
+                                        IconButton(
+                                          onPressed: () {},
+                                          icon: const Icon(
+                                            Icons.close,
+                                            color: Colors.white,
+                                            size: 24,
+                                          ),
+                                        ),
+                                        Slider.adaptive(
+                                          // inactiveColor: Colors.white,
+                                          min: 0,
+                                          max: 100,
+                                          value: value,
+
+                                          onChanged: (value) {
+                                            change(value);
+                                          },
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            decoration: const BoxDecoration(
+                              color: AppColors.lightBlack,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                              ),
+                            ),
+                            height: 147,
+                          ),
+                        );
+                      },
+                      icon: const Icon(
+                        CupertinoIcons.play,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                    ),
+                    title: const Text(
+                      "My Recording",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: const Text(
+                      "2/01/2001",
+                      style: TextStyle(
+                        color: Color(0xff484848),
+                      ),
+                    ),
+                    trailing: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.more_horiz,
+                        color: Color(0xff484848),
+                        size: 24,
+                      ),
+                    ),
+                  ),
                 ),
                 separatorBuilder: (context, index) => const Divider(
                   color: Colors.white,
@@ -69,5 +167,13 @@ class AllRecordingsScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+   double value = 0;
+
+  change(double val) {
+    setState(() {
+      value = val;
+    });
   }
 }
